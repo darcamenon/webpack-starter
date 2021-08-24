b4314c0c6b02c80b9869d9f82b6cfd2cf9eebaee
David <darcamenon@outlook.com>
2021-08-23 20:27:29 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { saludar } from './js/componentes';
+import './styles.css';
+
+const apellido = 'Vargas'
+const nombre = 'Alberto';
+
+
+saludar(nombre, apellido);
---
+++ src/js/componentes.js
@@ -0,0 +1,19 @@
+import '../css/componentes.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
+
+
+
+export const saludar = (nombre, apellido) => {
+
+    console.log('Creando etiqueta h1');
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${nombre} ${apellido}`;
+    document.body.append(h1);
+
+
+    // Img 
+
+    // const img = document.createElement('img');
+    // img.src = webpacklogo;
+    // document.body.append(img)
+}
